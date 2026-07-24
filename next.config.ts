6099ef3933e3082ef9faa5e10bcf45512e461ccd
RoiLaboratories <roilaboratories@gmail.com>
2026-07-24 16:46:18 +0100

    app updated

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+   output: "standalone",
   images: {
     domains: [
       "udebjfrhnwqoawziuhgu.supabase.co", // Supabase storage domain
